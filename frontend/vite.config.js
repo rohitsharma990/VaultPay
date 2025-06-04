b93968d38859da531798a7a4acb2651196a4338e
Rohit <rohit15sharmaa@gmail.com>
2025-06-04 21:16:11 +0530

    🔧 Update environment configuration, add Tailwind CSS support, and enhance build scripts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ]
+  plugins: [react()],
+  base: '/', // 💡 Important for GitHub Pages
 })
